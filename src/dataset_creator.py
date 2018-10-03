93f176edfc30ab14df2c8913666e791a454f6e40
Peter Szakacs <petersz256@gmail.com>
2018-10-03 16:07:33 +0200

    Refactored dataset_creator to output all 3 projections

--- src/dataset_creator.py
+++ src/dataset_creator.py
@@ -4,8 +4,9 @@ import csv
 
 import numpy as np
 
-import utils.packets.packet_utils as pack
+import utils.data_utils as dat
 import utils.io_utils as iout
+import utils.packets.packet_utils as pack
 import cmdint.cmd_interface_creator as cmd
 
 
@@ -20,17 +21,23 @@ import cmdint.cmd_interface_creator as cmd
 # for creating projections from the extracted packets, targets and metadata for them
 def on_packet_extracted_flight(packet, packet_idx, srcfile):
     global data, targets, metadata_dict
-    data.append(packet_template.create_x_y_projection(packet, start_idx=27, end_idx=47))
+    data[0].append(manipulator.create_x_y_projection(packet, start_idx=27, end_idx=47))
+    data[1].append(manipulator.create_x_gtu_projection(packet, start_idx=27, end_idx=47))
+    data[2].append(manipulator.create_y_gtu_projection(packet, start_idx=27, end_idx=47))
     metadata_dict.append({'source_file_acquisition_full': srcfile, 'packet_idx': packet_idx})
     targets.append([0, 1])
 
 def on_packet_extracted_simu(packet, packet_idx, srcfile):
     if (packet_idx == 1):
         global data, targets, metadata_dict
-        data.append(packet_template.create_x_y_projection(packet, start_idx=27, end_idx=47))
+        data[0].append(manipulator.create_x_y_projection(packet, start_idx=27, end_idx=47))
+        data[1].append(manipulator.create_x_gtu_projection(packet, start_idx=27, end_idx=47))
+        data[2].append(manipulator.create_y_gtu_projection(packet, start_idx=27, end_idx=47))
         metadata_dict.append({'source_file_acquisition_full': srcfile, 'packet_idx': packet_idx})
         targets.append([1, 0])
-        data.append(packet_template.create_x_y_projection(packet, start_idx=0, end_idx=20))
+        data[0].append(manipulator.create_x_y_projection(packet, start_idx=0, end_idx=20))
+        data[1].append(manipulator.create_x_gtu_projection(packet, start_idx=0, end_idx=20))
+        data[2].append(manipulator.create_y_gtu_projection(packet, start_idx=0, end_idx=20))
         metadata_dict.append({'source_file_acquisition_full': srcfile, 'packet_idx': packet_idx})
         targets.append([0, 1])
 
@@ -45,8 +52,9 @@ EC_width, EC_height = 16, 16
 frame_width, frame_height = 48, 48
 num_frames_per_packet = 128
 
-data, targets, metadata_dict = [], [], []
+data, targets, metadata_dict = ([], [], []), [], []
 packet_template = pack.packet_template(EC_width, EC_height, frame_width, frame_height, num_frames_per_packet)
+manipulator = dat.packet_manipulator(packet_template, verify_against_template=True)
 extractor = iout.packet_extractor(packet_template=packet_template)
 
 if args.simu:
@@ -61,7 +69,7 @@ else:
 input_tsv = args.filelist
 output_tsv = os.path.join(args.outdir, args.name + '_meta.tsv')
 total_num_data = 0
-X, Y = [], []
+YX_proj, GTU_X_proj, GTU_Y_proj, Y = [], [], [], []
 # main loop
 with open(input_tsv, 'r') as infile, open(output_tsv, 'w') as outfile:
     reader = csv.DictReader(infile, delimiter='\t')
@@ -71,13 +79,17 @@ with open(input_tsv, 'r') as infile, open(output_tsv, 'w') as outfile:
         srcfile, triggerfile = row['source_file_acquisition_full'], None
         print("Processing file {}".format(srcfile))
         data_extractor(srcfile, triggerfile)
-        X.extend(data)
+        YX_proj.extend(data[0])
+        GTU_X_proj.extend(data[1])
+        GTU_Y_proj.extend(data[2])
         Y.extend(targets)
-        num_data = len(data)
+        num_data = len(data[0])
         total_num_data += num_data
         print("Extracted: {} data items".format(num_data))
         print("Dataset current total data items count: {}".format(total_num_data))
-        data.clear()
+        data[0].clear()
+        data[1].clear()
+        data[2].clear()
         targets.clear()
     # write file metadata out all at once
     writer.writerows(metadata_dict)
@@ -86,7 +98,12 @@ print('Creating dataset "{}" containing {} items'.format(args.name, total_num_da
 
 # save dataset
 
-out_x = os.path.join(args.outdir, args.name + '_x')
+out_y_x = os.path.join(args.outdir, args.name + '_yx')
+out_gtu_x = os.path.join(args.outdir, args.name + '_gtux')
+out_gtu_y = os.path.join(args.outdir, args.name + '_gtuy')
+np.save(out_y_x, YX_proj)
+np.save(out_gtu_x, GTU_X_proj)
+np.save(out_gtu_y, GTU_Y_proj)
+
 out_y = os.path.join(args.outdir, args.name + '_y')
-np.save(out_x, X)
 np.save(out_y, Y)
