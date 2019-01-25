e97fd39c494407d6757efe77feb06e85b65e40cb
Peter Szakacs <petersz256@gmail.com>
2019-01-25 16:40:00 +0100

    Argument grouping and minor cleanup in dataset_merger

--- src/cmdint/cmd_interface_merger.py
+++ src/cmdint/cmd_interface_merger.py
@@ -6,35 +6,36 @@ import cmdint.common_args as cargs
 class cmd_interface():
 
     def __init__(self):
-        self.parser = argparse.ArgumentParser(
+        parser = argparse.ArgumentParser(
             description=('Merge multiple datasets into a single dataset by '
                          'concatenating them in the order they are passed'))
         in_aliases = {'dataset': 'dataset'}
         out_aliases = {'dataset name': 'name', 'dataset directory': 'outdir'}
-        self.dset_args = cargs.dataset_args(input_aliases=in_aliases,
-                                              output_aliases=out_aliases)
-        self.dset_args.add_dataset_arg_single(self.parser,
-                                              cargs.arg_type.INPUT,
-                                              short_alias='d',
-                                              multiple=True)
-        self.dset_args.add_dataset_arg_double(self.parser,
-                                              cargs.arg_type.OUTPUT,
-                                              name_short_alias='n',
-                                              dir_short_alias='o',
-                                              dir_default='.')
-        self.parser.add_argument('--delete_original', action='store_true',
-                                help='delete the original input datasets')
+        dset_args = cargs.dataset_args(input_aliases=in_aliases,
+                                       output_aliases=out_aliases)
 
+        group = parser.add_argument_group(title="Input dataset settings")
+        dset_args.add_dataset_arg_single(group, cargs.arg_type.INPUT,
+                                         short_alias='d', multiple=True)
+
+        group = parser.add_argument_group(title="Output dataset settings")
+        dset_args.add_dataset_arg_double(group, cargs.arg_type.OUTPUT,
+                                         dir_short_alias='o', dir_default='.',
+                                         name_short_alias='n')
+        # group.add_argument('--delete_original', action='store_true',
+        #                    help='delete the original input datasets')
+        self.parser = parser
+        self.dset_args = dset_args
 
     def get_cmd_args(self, argsToParse):
         args = self.parser.parse_args(argsToParse)
 
-        datasets = self.dset_args.get_dataset_single(args, 
+        datasets = self.dset_args.get_dataset_single(args,
                                                      cargs.arg_type.INPUT)
         if len(datasets) < 2:
             raise ValueError("At least 2 datasets must be passed")
 
-        name, outdir = self.dset_args.get_dataset_double(args, 
+        name, outdir = self.dset_args.get_dataset_double(args,
                                                          cargs.arg_type.OUTPUT)
         if not os.path.isdir(outdir):
             raise ValueError("Invalid output directory {}".format(args.outdir))
