bbaf3b167a4ee3ab8a6aee021ee9bd8870df5364
Peter Szakacs <petersz256@gmail.com>
2019-02-27 15:18:11 +0100

    Add configuration options for dataset mixin test classes
    Allow specifying the number of items, targets and metadata to create
    (at least 2).
    
    Also add new mixin class which creates an actual dataset from the
    items created in these mixins (the other 3 mixin classes are not
    inherited). Besides number of dataset items also allow specifying
    item_types and dataset name.

--- src/test/test_setups.py
+++ src/test/test_setups.py
@@ -4,6 +4,7 @@ import unittest
 import numpy as np
 
 import utils.data_utils as dat
+import utils.dataset_utils as ds
 import utils.metadata_utils as meta
 
 
@@ -14,17 +15,15 @@ class MockTextFileStream(io.StringIO):
         super(MockTextFileStream, self).__exit__(type, value, traceback)
 
 
-_NUM_PACKETS = 2
-
-
 class DatasetItemsMixin(unittest.TestCase):
 
     # test setup
 
     @classmethod
-    def setUpClass(cls):
+    def setUpClass(cls, num_items=2):
+        if (num_items < 2):
+            raise ValueError('Number of items must be at least 2')
         super(DatasetItemsMixin, cls).setUpClass()
-        n_packets = _NUM_PACKETS
         f_w, f_h, n_f = 48, 64, 20
         packet_shape = (n_f, f_h, f_w)
         item_shapes = {
@@ -34,10 +33,10 @@ class DatasetItemsMixin(unittest.TestCase):
             'gtuy': (n_f, f_h),
         }
         items = {
-            'raw' : np.ones((n_packets, *item_shapes['raw'])),
-            'yx'  : np.ones((n_packets, *item_shapes['yx'])),
-            'gtux': np.ones((n_packets, *item_shapes['gtux'])),
-            'gtuy': np.ones((n_packets, *item_shapes['gtuy']))}
+            'raw' : np.ones((num_items, *item_shapes['raw'])),
+            'yx'  : np.ones((num_items, *item_shapes['yx'])),
+            'gtux': np.ones((num_items, *item_shapes['gtux'])),
+            'gtuy': np.ones((num_items, *item_shapes['gtuy']))}
         item_types = {k: True for k in dat.ALL_ITEM_TYPES}
         # make some randomness in the first packet and derived items
         packet = items['raw'][0]
@@ -46,7 +45,7 @@ class DatasetItemsMixin(unittest.TestCase):
         yx.fill(4)
         gtux, gtuy = items['gtux'][0], items['gtuy'][0]
         gtux[0, 0], gtuy[0, 0], gtux[1], gtuy[1] = 3, 3, 4, 4
-        cls.n_packets = n_packets
+        cls.n_packets = num_items
         cls.n_f, cls.f_h, cls.f_w = n_f, f_h, f_w
         cls.packet_shape = (n_f, f_h, f_w)
         cls.item_shapes = item_shapes
@@ -57,18 +56,44 @@ class DatasetItemsMixin(unittest.TestCase):
 class DatasetTargetsMixin(unittest.TestCase):
 
     @classmethod
-    def setUpClass(cls):
+    def setUpClass(cls, num_items=2):
+        if (num_items < 2):
+            raise ValueError('Number of items must be at least 2')
         super(DatasetTargetsMixin, cls).setUpClass()
-        n_targets = _NUM_PACKETS
-        cls.mock_targets = [[0, 0], [0, 1], ]
+        cls.mock_targets = [[0, 0] if idx % 2 == 0 else [0, 1]
+                            for idx in range(num_items)]
 
 
 class DatasetMetadataMixin(unittest.TestCase):
 
     @classmethod
-    def setUpClass(cls):
+    def setUpClass(cls, num_items=2):
+        if (num_items < 2):
+            raise ValueError('Number of items must be at least 2')
         super(DatasetMetadataMixin, cls).setUpClass()
-        n_meta = _NUM_PACKETS
         meta_dict = {k: None for k in meta.FLIGHT_METADATA}
-        cls.mock_meta = [meta_dict.copy() for idx in range(n_meta)]
+        cls.mock_meta = [meta_dict.copy() for idx in range(num_items)]
         cls.metafields = set(meta.FLIGHT_METADATA)
+
+
+class DatasetMixin():
+
+    @classmethod
+    def setUpClass(cls, num_items=2, name='test', item_types=None):
+        items_mixin = DatasetItemsMixin()
+        items_mixin.setUpClass(num_items=num_items)
+        targets_mixin = DatasetTargetsMixin()
+        targets_mixin.setUpClass(num_items=num_items)
+        meta_mixin = DatasetMetadataMixin()
+        meta_mixin.setUpClass(num_items=num_items)
+        packets = items_mixin.items['raw']
+        targets = targets_mixin.mock_targets
+        metadata = meta_mixin.mock_meta
+
+        if item_types is None:
+            item_types = items_mixin.item_types
+        dset = ds.numpy_dataset(name, items_mixin.packet_shape,
+                                item_types=item_types)
+        for idx in range(num_items):
+            dset.add_data_item(packets[idx], targets[idx], metadata[idx])
+        cls.dset = dset
