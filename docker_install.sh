374539c6654e0d6e4b8679c80d9b2697771a04ef
peter.szakacs <petersz256@gmail.com>
2019-12-13 22:18:58 +0100

    Experimental: Add docker build file

---
+++ docker_install.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+INSTALL_DIR=$1
+
+SCRIPTS=($(find "$INSTALL_DIR/src" -maxdepth 1 -type f -name *.py | sort))
+
+for SCRIPT in "${SCRIPTS[@]}"; do
+	BASENAME=$(basename $SCRIPT | sed -e "s/.py$//g")
+	printf "#!/bin/bash\n\npython3 $SCRIPT \"\$@\"" > "/usr/bin/$BASENAME"
+	chmod +x "/usr/bin/$BASENAME"
+done
+
