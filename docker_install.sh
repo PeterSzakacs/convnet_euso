626662a815695a8a1e93339ec6cc34ccb3f81018
peter.szakacs <petersz256@gmail.com>
2019-12-20 22:28:00 +0100

    Experimental: Refine script to setup tools within image.
    - Use /tmp for tensorflow logs
    - Refactor scripts to use recommended bash syntax.

--- docker_install.sh
+++ docker_install.sh
@@ -2,12 +2,19 @@
 
 INSTALL_DIR=$1
 
-SCRIPTS=($(find "$INSTALL_DIR/src" -maxdepth 1 -type f -name *.py | sort))
+# get all python scripts in the src directory
+mapfile -t SCRIPTS < <(find "$INSTALL_DIR/src" -maxdepth 1 -type f -name "*.py" | sort)
 
+# create wrapper scripts in /usr/bin which call the individual scripts
 for SCRIPT in "${SCRIPTS[@]}"; do
-	BASENAME=$(basename $SCRIPT | sed -e "s/.py$//g")
-	printf "#!/bin/bash\n\npython3 $SCRIPT \"\$@\"" > "/usr/bin/$BASENAME"
+	BASENAME=$(basename "$SCRIPT" | sed -e "s/.py$//g")
+	printf "#!/bin/bash\n\npython3 %s \"\$@\"" "$SCRIPT" > "/usr/bin/$BASENAME"
 	chmod +x "/usr/bin/$BASENAME"
 done
 
-cp $INSTALL_DIR/config/unix/* $HOME
+# copy configs from config/unix to the user home directory
+for CONFIGFILE in "$INSTALL_DIR"/config/unix/*.ini; do
+  CONFIG_FILENAME=$(basename "$CONFIGFILE")
+  # $XDG_RUNTIME_DIR is not set within image, just use /tmp for tensorflow logs
+  sed -e "s&logdir=.*&logdir=/tmp&g" "$CONFIGFILE" > "$HOME/$CONFIG_FILENAME"
+done
