f34e5f1e0702dfaf726232f307154baa83f8fe3d
peter.szakacs <petersz256@gmail.com>
2020-02-19 20:36:06 +0100

    Dataset: WIP - Implement new dataset solution (data items facade)

---
+++ src/dataset/data/facades.py
@@ -0,0 +1,116 @@
+import dataset.data.constants as cons
+import dataset.data.utils as utils
+
+
+class DataFacade:
+    """
+    Facade class providing a container for packet data-derived items in a
+    tagged dataset and some additional operations on them.
+
+    :param packet_shape: shape of the original packets from which the passed
+                         data items were derived, represented as a tuple of
+                         (number of frames, frame height, frame width)
+    :type packet_shape: tuple of int
+    :param items_dict: data items to store in this facade instance
+    :type items_dict: dict of str,numpy.ndarray
+    :param copy_on_read: optional flag indicating if item retrieval should
+                         return a copy of the stored items instead of the
+                         original (default: False)
+    :type copy_on_read: bool
+    """
+
+    def __init__(self, packet_shape, items_dict, copy_on_read=False):
+        packet_shape = tuple(packet_shape)
+        items = {k: v for k, v in items_dict.items() if v is not None}
+        item_types = {k: True for k, v in items.items()}
+
+        utils.check_item_types(item_types)
+        utils.check_items_length(items)
+
+        self._used_types = tuple(k for k in cons.ALL_ITEM_TYPES
+                                 if item_types.get(k, False) is True)
+        self._packet_shape = packet_shape
+        self._item_types = item_types
+        self._data = items
+        self._num_items = len(list(items.values())[0])
+        self.copy_on_read = copy_on_read
+
+    def __len__(self):
+        return self._num_items
+
+    # properties
+
+    @property
+    def item_types(self):
+        """
+            The type of items in this dataset, as a dict of str to bool, where
+            the all the keys are from the 'ALL_ITEM_TYPES' module constant and
+            the values represent wheher a collection of items of this type is
+            present in this dataset.
+        """
+        return self._item_types
+
+    @property
+    def packet_shape(self):
+        """
+            The shape of accepted raw packets to be converted to dataset items.
+        """
+        return self._packet_shape
+
+    @property
+    def item_shapes(self):
+        """
+            The shape of individual item types in this dataset, in form of a
+            dict of str to tuple of int, where the keys are from the module
+            constant 'ALL_ITEM_TYPES' and the values represent the shape of
+            items as they would be presented from the items themselves by
+            getting the numpy.ndarray 'shape' property.
+        """
+        return {k: item.shape[1:] for k, item in self._data.items()}
+
+    # methods
+
+    def get_data_as_tuple(self, idx=None):
+        """Return data items contained in this facade instance as a tuple of
+        numpy.ndarrays ordered by their type as defined in ALL_ITEM_TYPES.
+
+        :param idx: optional indexing parameter
+        :type idx: None or int or range or Sequence of int
+        :return: tuple of numpy.ndarray
+        """
+        data, idxs = self._data, self._get_indexes(idx)
+        if self.copy_on_read:
+            return tuple(data[k][idxs].copy() for k in self._used_types)
+        else:
+            return tuple(data[k][idxs] for k in self._used_types)
+
+    def get_data_as_dict(self, idx=None):
+        """Return data items contained in this facade instance as a dict of
+        str to numpy.ndarray.
+
+        Note that only keys for item types contained in this facade have
+        defined values, keys for other item types are not even present.
+
+        :param idx: optional indexing parameter
+        :type idx: None or int or range or Sequence of int
+        :return: dict of (str,numpy.ndarray)
+        """
+        data, idxs = self._data, self._get_indexes(idx)
+        if self.copy_on_read:
+            return {k: data[k][idxs].copy() for k in self._used_types}
+        else:
+            return {k: data[k][idxs] for k in self._used_types}
+
+    def shuffle(self, shuffler, shuffler_state_resetter):
+        for item_type in self._used_types:
+            items = self._data[item_type]
+            shuffler(items)
+            shuffler_state_resetter()
+
+    # helper methods
+
+    def _get_indexes(self, indexing_obj):
+        if indexing_obj is None:
+            return range(self._num_items)
+        else:
+            return indexing_obj
---
+++ src/dataset/data/test/test_facades.py
@@ -0,0 +1,167 @@
+import unittest
+
+import numpy as np
+import numpy.testing as nptest
+
+import dataset.data.facades as facades
+import dataset.data.test.utils as test_utils
+
+
+class TestDataFacade(test_utils.DataItemsDictUtilsMixin):
+
+    # custom asserts
+
+    def assertItemsTupleEqual(self, items, exp_items):
+        self.assertEqual(len(items), len(exp_items))
+        for idx in range(len(exp_items)):
+            nptest.assert_array_equal(items[idx], exp_items[idx])
+
+    # test setup
+
+    @classmethod
+    def setUpClass(cls):
+        num_items, dtype = 8, np.uint8
+        packets = np.ones((num_items, 100, 10, 20), dtype=dtype)
+        items = {
+            "raw": packets,
+            "yx": np.ones((num_items, 10, 20), dtype=dtype),
+            "gtux": np.ones((num_items, 100, 20), dtype=dtype),
+            "gtuy": np.ones((num_items, 100, 10), dtype=dtype),
+        }
+        cls.num_items = num_items
+        cls.dtype = dtype
+        cls.packet_shape = packets[0].shape
+        cls.items = items
+        cls.item_shapes = {k: item[0].shape for k, item in items.items()}
+
+    # test facade properties
+
+    def test_item_types(self):
+        included_types = ('yx', 'gtux')
+        items = {k: self.items[k] for k in included_types}
+        exp_types = {k: True for k in included_types}
+
+        facade = facades.DataFacade(self.packet_shape, items)
+        self.assertDictEqual(facade.item_types, exp_types)
+
+    def test_item_shapes(self):
+        included_types = ('yx', 'gtux', 'gtuy')
+        items = {k: self.items[k] for k in included_types}
+        exp_shapes = {k: self.item_shapes[k] for k in included_types}
+
+        facade = facades.DataFacade(self.packet_shape, items)
+        self.assertDictEqual(facade.item_shapes, exp_shapes)
+
+    def test_packet_shape(self):
+        facade = facades.DataFacade(self.packet_shape, self.items)
+        self.assertTupleEqual(facade.packet_shape, self.packet_shape)
+
+    def test_length(self):
+        included_types = ('yx', 'gtux')
+        items = {k: self.items[k] for k in included_types}
+
+        facade = facades.DataFacade(self.packet_shape, items)
+        self.assertEqual(len(facade), self.num_items)
+
+    # test item retrieval methods (no copy on read)
+
+    def test_get_data_as_dict(self):
+        included_types = ('raw', 'yx')
+        items = self.items
+        in_items = {k: items[k] for k in included_types}
+        exp_items = {k: items[k] for k in included_types}
+
+        facade = facades.DataFacade(self.packet_shape, in_items)
+        self.assertItemsDictEqual(facade.get_data_as_dict(), exp_items)
+
+    def test_get_data_as_dict_with_slice(self):
+        included_types = ('yx', 'gtux', 'gtuy')
+        items = self.items
+        in_items = {k: items[k] for k in included_types}
+        exp_items = {k: items[k][0:2] for k in included_types}
+
+        facade = facades.DataFacade(self.packet_shape, in_items)
+        self.assertItemsDictEqual(facade.get_data_as_dict(slice(0, 2)),
+                                  exp_items)
+
+    def test_get_data_as_dict_with_range(self):
+        included_types = ('raw', 'yx')
+        items = self.items
+        in_items = {k: items[k] for k in included_types}
+        exp_items = {k: items[k][range(1, 3)] for k in included_types}
+
+        facade = facades.DataFacade(self.packet_shape, in_items)
+        self.assertItemsDictEqual(facade.get_data_as_dict(range(1, 3)),
+                                  exp_items)
+
+    def test_get_data_as_dict_with_indexes_list(self):
+        included_types = ('yx',)
+        items = self.items
+        in_items = {k: items[k] for k in included_types}
+        exp_items = {k: items[k][1, 4, 7] for k in included_types}
+
+        facade = facades.DataFacade(self.packet_shape, in_items)
+        self.assertItemsDictEqual(facade.get_data_as_dict([1, 4, 7]),
+                                  exp_items)
+
+    def test_get_data_as_arraylike(self):
+        included_types = ('yx',)
+        items = self.items
+        in_items = {k: items[k] for k in included_types}
+        exp_items = tuple(items[k] for k in included_types)
+
+        facade = facades.DataFacade(self.packet_shape, in_items)
+        self.assertItemsTupleEqual(facade.get_data_as_tuple(), exp_items)
+
+    def test_get_data_as_arraylike_with_slice(self):
+        included_types = ('yx', 'gtux', 'gtuy')
+        items = self.items
+        in_items = {k: items[k] for k in included_types}
+        exp_items = tuple(items[k][4:6] for k in included_types)
+
+        facade = facades.DataFacade(self.packet_shape, in_items)
+        self.assertItemsTupleEqual(facade.get_data_as_tuple(slice(4, 6)),
+                                   exp_items)
+
+    def test_get_data_as_arraylike_with_range(self):
+        included_types = ('gtux',)
+        items = self.items
+        in_items = {k: items[k] for k in included_types}
+        exp_items = tuple(items[k][range(3, 7)] for k in included_types)
+
+        facade = facades.DataFacade(self.packet_shape, in_items)
+        self.assertItemsTupleEqual(facade.get_data_as_tuple(range(3, 7)),
+                                   exp_items)
+
+    def test_get_data_as_arraylike_with_indexes_list(self):
+        included_types = ('gtux', 'gtuy',)
+        items = self.items
+        in_items = {k: items[k] for k in included_types}
+        exp_items = tuple(items[k][1, 6, 3] for k in included_types)
+
+        facade = facades.DataFacade(self.packet_shape, in_items)
+        self.assertItemsTupleEqual(facade.get_data_as_tuple((1, 6, 3)),
+                                   exp_items)
+
+    # test item shuffling
+
+    def test_shuffle(self):
+        def shuffler(seq):
+            # set items[0] to all zeroes and items[3] to all sevens
+            seq[0] = 0
+            seq[3] = 7
+
+        included_types = ('raw', 'yx')
+        items = self.items
+        in_items = {k: items[k] for k in included_types}
+        exp_items = {k: items[k].copy() for k in included_types}
+        shuffler(exp_items['raw'])
+        shuffler(exp_items['yx'])
+
+        facade = facades.DataFacade(self.packet_shape, in_items)
+        facade.shuffle(shuffler, lambda: None)
+        self.assertItemsDictEqual(facade.get_data_as_dict(), exp_items)
+
+
+if __name__ == '__main__':
+    unittest.main()
