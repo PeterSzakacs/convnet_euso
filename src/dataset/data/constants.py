5232f7a94ccfd7f37f31eb732cf1b8cc374dd886
peter.szakacs <petersz256@gmail.com>
2020-02-19 20:34:16 +0100

    Dataset: WIP - implement new dataset solution (data constants)

---
+++ src/dataset/data/constants.py
@@ -0,0 +1,5 @@
+# defined legal types of data items in a dataset and their default order
+# currently only raw data packets and projections along one of the 3 axes
+# are supported
+
+ALL_ITEM_TYPES = ('raw', 'yx', 'gtux', 'gtuy')
