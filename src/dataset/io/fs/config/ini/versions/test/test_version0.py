b044119676d449b48121152afae70c3087b17e82
peter.szakacs <petersz256@gmail.com>
2020-06-20 15:33:51 +0200

    Dataset (IO) - extend backend config for version 0
    - Add per-category settable property to specify filename extension of holding files
    - Add per-category settable property to specify filename format of holding files
    - Add support for category-agnostic default settings (currently only for filename format)
    - Also fix base class for version 0 INI config parser
    
    Also split parser unit tests for better readability of errors and add test case descriptions

--- src/dataset/io/fs/config/ini/versions/test/test_version0.py
+++ src/dataset/io/fs/config/ini/versions/test/test_version0.py
@@ -3,14 +3,19 @@ import unittest
 import dataset.io.fs.config.ini.versions.version0 as v0
 
 
-class TestConfigParser(unittest.TestCase):
+class TestLegacyConfigParsing(unittest.TestCase):
+
+    def shortDescription(self):
+        return """
+        Test parsing of legacy configs without a version attribute 
+        (other attributes are implicitly set to defaults)
+        """
 
     @classmethod
     def setUp(cls):
         cls.maxDiff = None
-
-    def test_parse_unversioned_config(self):
-        raw_config = {
+        cls.parser = v0.ConfigParser()
+        cls.raw_config = {
             'general': {
                 'num_data': '100', 'dtype': 'uint8',
                 'metafields': "{'bg_lambda', 'length', 'yx_angle'}",
@@ -23,12 +28,16 @@ class TestConfigParser(unittest.TestCase):
                 'gtux': 'False', 'gtuy': 'False'
             },
         }
-        exp_config = {
+        cls.exp_config = {
             'version': 0,
             'num_items': 100,
             'data': {
                 'packet_shape': (20, 24, 48),
-                'backend': 'npy',
+                'backend': {
+                    'name': 'npy',
+                    'filename_extension': 'npy',
+                    'filename_format': 'name_with_type_suffix',
+                },
                 'types': {
                     'raw': {
                         'dtype': 'uint8'
@@ -39,7 +48,11 @@ class TestConfigParser(unittest.TestCase):
                 },
             },
             'targets': {
-                'backend': 'npy',
+                'backend': {
+                    'name': 'npy',
+                    'filename_extension': 'npy',
+                    'filename_format': 'name_with_type_suffix',
+                },
                 'types': {
                     'softmax_class_value': {
                         'dtype': 'uint8'
@@ -47,24 +60,69 @@ class TestConfigParser(unittest.TestCase):
                 },
             },
             'metadata': {
+                'backend': {
+                    'name': 'tsv',
+                    'filename_extension': 'tsv',
+                    'filename_format': 'name_with_type_suffix',
+                },
                 'fields': {'bg_lambda', 'length', 'yx_angle'},
-                'backend': 'tsv',
             }
         }
-        parser = v0.ConfigParser()
-        config = parser.parse_config(raw_config)
-        self.assertDictEqual(exp_config, config)
 
-    def test_parse_versioned_config(self):
-        raw_config = {
+    def test_parse_unversioned_config_version(self):
+        config = self.parser.parse_config(self.raw_config)
+        self.assertIn('version', config)
+        self.assertEqual(config['version'], 0)
+
+    def test_parse_version0_config_num_items(self):
+        config = self.parser.parse_config(self.raw_config)
+        self.assertIn('num_items', config)
+        self.assertEqual(config['num_items'], self.exp_config['num_items'])
+
+    def test_parse_unversioned_config_data(self):
+        config = self.parser.parse_config(self.raw_config)
+        self.assertIn('data', config)
+        self.assertDictEqual(config['data'], self.exp_config['data'])
+
+    def test_parse_unversioned_config_targets(self):
+        config = self.parser.parse_config(self.raw_config)
+        self.assertIn('targets', config)
+        self.assertDictEqual(config['targets'], self.exp_config['targets'])
+
+    def test_parse_unversioned_config_metadata(self):
+        config = self.parser.parse_config(self.raw_config)
+        self.assertIn('metadata', config)
+        self.assertDictEqual(config['metadata'], self.exp_config['metadata'])
+
+
+class TestVersion0ConfigParsing(unittest.TestCase):
+
+    def shortDescription(self):
+        return """
+        Test parsing of configs with a version attribute set to 0
+        """
+
+    @classmethod
+    def setUp(cls):
+        cls.maxDiff = None
+        cls.parser = v0.ConfigParser()
+        cls.raw_config = {
             'general': {
                 'version': '0', 'num_items': '200',
             },
+            'general:backend': {
+                'filename_format': 'name_with_type_suffix',
+            },
             'data': {
-                'types': "{'yx', 'gtux', 'gtuy'}", 'backend': 'memmap',
+                'types': "{'yx', 'gtux', 'gtuy'}",
             },
             'data:packet_shape': {
                 'num_frames': '30', 'frame_height': '48', 'frame_width': '24',
+            },
+            'data:backend': {
+                'name': 'memmap',
+                'filename_extension': 'memmap',
+                'filename_format': 'type_only',
             },
             'data:yx': {
                 'dtype': 'uint8'
@@ -76,21 +134,33 @@ class TestConfigParser(unittest.TestCase):
                 'dtype': 'uint8'
             },
             'targets': {
-                'types': "{'softmax_class_value'}", 'backend': 'npy',
+                'types': "{'softmax_class_value'}",
+            },
+            'targets:backend': {
+                'name': 'npy',
+                'filename_extension': 'npy',
             },
             'targets:softmax_class_value': {
                 'dtype': 'uint8',
             },
             'metadata': {
-                'fields': "{'length', 'yx_angle'}", 'backend': 'tsv',
-            }
+                'fields': "{'length', 'yx_angle'}",
+            },
+            'metadata:backend': {
+                'name': 'tsv',
+                'filename_extension': 'tsv',
+            },
         }
-        exp_config = {
+        cls.exp_config = {
             'version': 0,
             'num_items': 200,
             'data': {
                 'packet_shape': (30, 48, 24),
-                'backend': 'memmap',
+                'backend': {
+                    'name': 'memmap',
+                    'filename_extension': 'memmap',
+                    'filename_format': 'type_only',
+                },
                 'types': {
                     'yx': {
                         'dtype': 'uint8'
@@ -104,7 +174,11 @@ class TestConfigParser(unittest.TestCase):
                 },
             },
             'targets': {
-                'backend': 'npy',
+                'backend': {
+                    'name': 'npy',
+                    'filename_extension': 'npy',
+                    'filename_format': 'name_with_type_suffix',
+                },
                 'types': {
                     'softmax_class_value': {
                         'dtype': 'uint8'
@@ -112,13 +186,39 @@ class TestConfigParser(unittest.TestCase):
                 },
             },
             'metadata': {
+                'backend': {
+                    'name': 'tsv',
+                    'filename_extension': 'tsv',
+                    'filename_format': 'name_with_type_suffix',
+                },
                 'fields': {'length', 'yx_angle'},
-                'backend': 'tsv',
             }
         }
-        parser = v0.ConfigParser()
-        config = parser.parse_config(raw_config)
-        self.assertDictEqual(exp_config, config)
+
+    def test_parse_version0_config_version(self):
+        config = self.parser.parse_config(self.raw_config)
+        self.assertIn('version', config)
+        self.assertEqual(config['version'], 0)
+
+    def test_parse_version0_config_num_items(self):
+        config = self.parser.parse_config(self.raw_config)
+        self.assertIn('num_items', config)
+        self.assertEqual(config['num_items'], self.exp_config['num_items'])
+
+    def test_parse_version0_config_data(self):
+        config = self.parser.parse_config(self.raw_config)
+        self.assertIn('data', config)
+        self.assertDictEqual(config['data'], self.exp_config['data'])
+
+    def test_parse_version0_config_targets(self):
+        config = self.parser.parse_config(self.raw_config)
+        self.assertIn('targets', config)
+        self.assertDictEqual(config['targets'], self.exp_config['targets'])
+
+    def test_parse_version0_config_metadata(self):
+        config = self.parser.parse_config(self.raw_config)
+        self.assertIn('metadata', config)
+        self.assertDictEqual(config['metadata'], self.exp_config['metadata'])
 
     def test_parse_unsupported_version(self):
         raw_config = {
@@ -129,8 +229,19 @@ class TestConfigParser(unittest.TestCase):
         parser = v0.ConfigParser()
         self.assertRaises(ValueError, parser.parse_config, raw_config)
 
-    def test_save_config(self):
-        dataset_attrs = {
+
+class TestVersion0ConfigCreation(unittest.TestCase):
+
+    def shortDescription(self):
+        return """
+        Test creation of config (version 0) dict for saving to FS
+        """
+
+    @classmethod
+    def setUp(cls):
+        cls.maxDiff = None
+        cls.parser = v0.ConfigParser()
+        cls.dataset_attrs = {
             'num_items': 200,
             'data': {
                 'types': {
@@ -145,7 +256,11 @@ class TestConfigParser(unittest.TestCase):
                     },
                 },
                 'packet_shape': (30, 48, 24),
-                'backend': 'memmap',
+                'backend': {
+                    'name': 'memmap',
+                    'filename_extension': 'memmap',
+                    'filename_format': 'type_only',
+                },
             },
             'targets': {
                 'types': {
@@ -153,19 +268,32 @@ class TestConfigParser(unittest.TestCase):
                         'dtype': 'uint8',
                     }
                 },
-                'backend': 'npy',
+                'backend': {
+                    'name': 'npy',
+                    'filename_extension': 'test',
+                    'filename_format': 'type_only',
+                },
             },
             'metadata': {
                 'fields': {'bg_lambda', 'yx_angle'},
-                'backend': 'tsv',
+                'backend': {
+                    'name': 'tsv',
+                    'filename_extension': 'tsv',
+                    'filename_format': 'name_with_type_suffix',
+                },
             }
         }
-        exp_config = {
+        cls.exp_config = {
             'general': {
                 'version': 0, 'num_items': 200,
             },
             'data': {
-                'types': {'gtux', 'yx', 'gtuy'}, 'backend': 'memmap',
+                'types': {'gtux', 'yx', 'gtuy'},
+            },
+            'data:backend': {
+                'name': 'memmap',
+                'filename_extension': 'memmap',
+                'filename_format': 'type_only',
             },
             'data:packet_shape': {
                 'num_frames': 30, 'frame_height': 48, 'frame_width': 24,
@@ -180,18 +308,67 @@ class TestConfigParser(unittest.TestCase):
                 'dtype': 'uint8',
             },
             'targets': {
-                'types': {'softmax_class_value'}, 'backend': 'npy',
+                'types': {'softmax_class_value'},
+            },
+            'targets:backend': {
+                'name': 'npy',
+                'filename_extension': 'test',
+                'filename_format': 'type_only',
             },
             'targets:softmax_class_value': {
                 'dtype': 'uint8',
             },
             'metadata': {
-                'fields': {'bg_lambda', 'yx_angle'}, 'backend': 'tsv',
+                'fields': {'bg_lambda', 'yx_angle'},
+            },
+            'metadata:backend': {
+                'name': 'tsv',
+                'filename_extension': 'tsv',
+                'filename_format': 'name_with_type_suffix',
             }
         }
-        parser = v0.ConfigParser()
-        config = parser.create_config(dataset_attrs)
-        self.assertDictEqual(exp_config, config)
+
+    def test_create_configv0_general_sec(self):
+        config = self.parser.create_config(self.dataset_attrs)
+        self.assertIn('general', config)
+        self.assertDictEqual(self.exp_config['general'], config['general'])
+
+    def test_create_configv0_data_sec(self):
+        input_attrs = self.dataset_attrs
+        conf = self.parser.create_config(input_attrs)
+        exp_conf = self.exp_config
+        self.assertIn('data', conf)
+        self.assertDictEqual(exp_conf['data'], conf['data'])
+        self.assertIn('data:backend', conf)
+        self.assertDictEqual(exp_conf['data:backend'], conf['data:backend'])
+        for item_type in input_attrs['data']['types']:
+            section_name = f'data:{item_type}'
+            self.assertIn(section_name, conf)
+            self.assertDictEqual(exp_conf[section_name], conf[section_name])
+
+    def test_create_configv0_targets_sec(self):
+        input_attrs = self.dataset_attrs
+        conf = self.parser.create_config(input_attrs)
+        exp_conf = self.exp_config
+        self.assertIn('targets', conf)
+        self.assertDictEqual(exp_conf['targets'], conf['targets'])
+        self.assertIn('targets:backend', conf)
+        self.assertDictEqual(
+            exp_conf['targets:backend'], conf['targets:backend'])
+        for item_type in input_attrs['targets']['types']:
+            section_name = f'targets:{item_type}'
+            self.assertIn(section_name, conf)
+            self.assertDictEqual(exp_conf[section_name], conf[section_name])
+
+    def test_create_configv0_metadata_sec(self):
+        input_attrs = self.dataset_attrs
+        conf = self.parser.create_config(input_attrs)
+        exp_conf = self.exp_config
+        self.assertIn('metadata', conf)
+        self.assertDictEqual(exp_conf['metadata'], conf['metadata'])
+        self.assertIn('metadata:backend', conf)
+        self.assertDictEqual(
+            exp_conf['metadata:backend'], conf['metadata:backend'])
 
 
 if __name__ == '__main__':
--- src/dataset/io/fs/config/ini/versions/version0.py
+++ src/dataset/io/fs/config/ini/versions/version0.py
@@ -1,26 +1,28 @@
 import ast
 
 import dataset.constants as cons
-import dataset.io.fs.base as fs_io_base
+import dataset.io.fs.config.ini.base as ini
 
 
-class ConfigParser(fs_io_base.FsPersistencyHandler):
+class ConfigParser(ini.AbstractIniConfigParser):
 
     @property
     def version(self):
         return 0
 
     def parse_config(self, config):
-        general_sec = config['general']
+        _conf = config.copy()
+        general_sec = _conf['general']
         version = general_sec.get('version', -1)
         if version == -1:
-            return self._parse_unversioned(config)
+            return self._parse_unversioned(_conf)
         elif version == '0':
-            return self._parse_versioned(config)
+            return self._parse_versioned(_conf)
         else:
             raise ValueError(f"Unsupported config version: {version}")
 
     def create_config(self, dataset_attrs):
+        dataset_attrs = dataset_attrs.copy()
         _data = dataset_attrs['data']
         _targets = dataset_attrs['targets']
         _metadata = dataset_attrs['metadata']
@@ -34,25 +36,25 @@ class ConfigParser(fs_io_base.FsPersistencyHandler):
             },
             'data': {
                 'types': set(_d_types),
-                'backend': _data['backend'],
             },
             'data:packet_shape': {
                 'num_frames': n_f,
                 'frame_height': f_h,
                 'frame_width': f_w,
             },
+            'data:backend': _data['backend'],
             **{f'data:{itype}': {'dtype': conf['dtype']}
                for itype, conf in _d_types.items()},
             'targets': {
                 'types': set(_t_types),
-                'backend': _targets['backend'],
             },
+            'targets:backend': _targets['backend'],
             **{f'targets:{itype}': {'dtype': conf['dtype']}
                for itype, conf in _t_types.items()},
             'metadata': {
-                'backend': _metadata['backend'],
                 'fields': _metadata['fields'],
-            }
+            },
+            'metadata:backend': _metadata['backend'],
         }
 
     # helper methods
@@ -75,32 +77,56 @@ class ConfigParser(fs_io_base.FsPersistencyHandler):
             'data': {
                 'packet_shape': (n_f, f_h, f_w),
                 'types': {itype: {'dtype': dtype} for itype in item_types},
-                'backend': 'npy',
+                'backend': {
+                    'name': 'npy',
+                    'filename_extension': 'npy',
+                    'filename_format': 'name_with_type_suffix',
+                }
             },
             'targets': {
                 'types': {'softmax_class_value': {'dtype': 'uint8'}},
-                'backend': 'npy',
+                'backend': {
+                    'name': 'npy',
+                    'filename_extension': 'npy',
+                    'filename_format': 'name_with_type_suffix',
+                },
             },
             'metadata': {
                 'fields': ast.literal_eval(general_sec['metafields']),
-                'backend': 'tsv',
+                'backend': {
+                    'name': 'tsv',
+                    'filename_extension': 'tsv',
+                    'filename_format': 'name_with_type_suffix',
+                },
             }
         }
 
     @staticmethod
     def _parse_versioned(config):
         general_sec = config['general']
+
         data_sec = config['data']
         packet_shape_sec = config['data:packet_shape']
+        data_backend_sec = config['data:backend']
+
         n_f = int(packet_shape_sec['num_frames'])
         f_h = int(packet_shape_sec['frame_height'])
         f_w = int(packet_shape_sec['frame_width'])
+
         targets_sec = config['targets']
+        targets_backend_sec = config['targets:backend']
+
         metadata_sec = config['metadata']
+        metadata_backend_sec = config['metadata:backend'] or {}
 
         d_types = ast.literal_eval(data_sec['types'])
         t_types = ast.literal_eval(targets_sec['types'])
 
+        backend_defaults = config.get('general:backend', {})
+        if backend_defaults:
+            data_backend_sec = {**backend_defaults, **data_backend_sec}
+            targets_backend_sec = {**backend_defaults, **targets_backend_sec}
+            metadata_backend_sec = {**backend_defaults, **metadata_backend_sec}
         return {
             'version': 0,
             'num_items': int(general_sec['num_items']),
@@ -110,17 +136,17 @@ class ConfigParser(fs_io_base.FsPersistencyHandler):
                     itype: {'dtype': config[f'data:{itype}']['dtype']}
                     for itype in d_types
                 },
-                'backend': data_sec['backend'],
+                'backend': data_backend_sec.copy(),
             },
             'targets': {
                 'types': {
                     itype: {'dtype': config[f'targets:{itype}']['dtype']}
                     for itype in t_types
                 },
-                'backend': targets_sec['backend'],
+                'backend': targets_backend_sec.copy(),
             },
             'metadata': {
                 'fields': ast.literal_eval(metadata_sec['fields']),
-                'backend': metadata_sec['backend'],
+                'backend': metadata_backend_sec.copy(),
             }
         }
